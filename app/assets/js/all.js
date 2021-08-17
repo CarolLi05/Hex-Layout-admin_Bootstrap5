2c76acb7718802f1c0d710939fb56c200530a8fa
Carol <dinosaur0514@gmail.com>
2021-08-17 23:42:20 +0800

    finish index, add admin table

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,20 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+// CK Editor
+ClassicEditor
+  .create( document.querySelector( '#editor' ) )
+  .then( editor => {
+  console.log( editor );
+} )
+.catch( error => {
+  console.error( error );
+} );
+
+// CK Editor Toggle
+$(document).ready(function(){
+  $('#reply_btn').click(function(){
+      $('.editorWrap').toggleClass('active');
+  });
+});
