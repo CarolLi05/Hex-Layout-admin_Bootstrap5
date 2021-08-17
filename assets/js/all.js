21225eea9c0a8a8be0d616ac4ac2e3bdd8f76ba9
Carol <dinosaur0514@gmail.com>
2021-08-17 23:39:49 +0800

    Update 2021-08-17T15:39:13.707Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,17 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
+}); // CK Editor
+
+ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
+  console.log(editor);
+})["catch"](function (error) {
+  console.error(error);
+}); // CK Editor Toggle
+
+$(document).ready(function () {
+  $('#reply_btn').click(function () {
+    $('.editorWrap').toggleClass('active');
+  });
 });
 //# sourceMappingURL=all.js.map
